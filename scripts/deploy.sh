1139822ef651e33943cda342f8ec9fb8aabe7f25
Brian Hicks <brian@brianthicks.com>
2021-05-10 13:16:46 -0500

    add a bit more to the deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,3 +7,16 @@ if test -z "$SSH_TARGET"; then
   echo "usage: ${0:-} SSH_TARGET"
   exit 1
 fi
+
+echo "== COPYING CODE"
+rsync -rv --filter ':- .gitignore' --exclude '/.git' --exclude '/.direnv' . "$SSH_TARGET:~/inky-pal"
+
+echo
+echo "== SETTING UP VENV"
+VENV_LOC=/home/pi/inky-pal-venv
+ssh "$SSH_TARGET" "test -d venv || python3 -m venv $VENV_LOC"
+
+echo
+echo "== INSTALLING DEPS"
+scp requirements.txt "$SSH_TARGET:~/requirements.txt"
+ssh "$SSH_TARGET" "$VENV_LOC/bin/pip install -r ~/requirements.txt"
